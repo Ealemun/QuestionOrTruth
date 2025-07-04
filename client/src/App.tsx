2f27d8e266df72d309907d2986ccf2967f035458
Labib Boubaker <labib@masteur.com>
2025-07-04 22:52:08 +0200

    Branchement des roomscreen et menuscreen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,35 @@
 import './App.css';
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from './components/LanguageSwitcher';
-import RoomSetup from './RoomSetup';
-
+import MenuScreen from './MenuScreen';
+import RoomScreen from './RoomScreen';
+import { useState } from 'react';
 
 function App() {
   const { t } = useTranslation();
+  const [playerName, setPlayerName] = useState('');
+  const [room, setRoom] = useState<any>(null);
+
+  const handleRoomJoin = (roomData: any, name: string) => {
+    setPlayerName(name);
+    setRoom(roomData);
+  };
+
+  const leaveRoom = () => {
+    setRoom(null);
+  };
 
   return (
-    <div className="App">
+    <div className="App p-4">
       <LanguageSwitcher />
       <h1>{t('title')}</h1>
       <p>{t('welcome')}</p>
-      <RoomSetup />
+
+      {!room ? (
+        <MenuScreen onRoomJoined={handleRoomJoin} />
+      ) : (
+        <RoomScreen room={room} playerName={playerName} onLeave={leaveRoom} />
+      )}
     </div>
   );
 }
