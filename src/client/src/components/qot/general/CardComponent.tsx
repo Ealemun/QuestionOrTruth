aadd325946092c5c276421ff9e71d9c4152e0b8a
Ealemun <labib.boubaker@gmail.com>
2025-08-10 18:23:10 +0200

    Ajout d'une marge entre les cartes dans CardComponent

--- src/client/src/components/qot/general/CardComponent.tsx
+++ src/client/src/components/qot/general/CardComponent.tsx
@@ -9,7 +9,7 @@ export const CardComponent = ({card, disabled}: Props) => {
     const path_card = `../graphics/single_cards/${card.suit}-${card.rank}.svg`
     // select gamephase from redux -> if != setup make the component non clickable
     return (
-    <div className="w-[70px] h-[100px] border-2 border-black">
+    <div className="w-[70px] m-[5px] border-2 border-black">
         <div className="flex flex-col">
             {/* <div>{card.suit}</div>
             <div>{card.rank}</div> */}
