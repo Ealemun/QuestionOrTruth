8ec7401c13c63288fca164763fd01f3a5c001862
Ealemun <labib.boubaker@gmail.com>
2025-08-10 18:12:31 +0200

    ajout des visuels pour les cartes dans CardComponent

--- src/client/src/components/qot/general/CardComponent.tsx
+++ src/client/src/components/qot/general/CardComponent.tsx
@@ -6,13 +6,22 @@ interface Props {
 }
 
 export const CardComponent = ({card, disabled}: Props) => {
-
+    const path_card = `../graphics/single_cards/${card.suit}-${card.rank}.svg`
     // select gamephase from redux -> if != setup make the component non clickable
     return (
-    <div className="w-[300px] h-[100px] border-2 border-black">
+    <div className="w-[70px] h-[100px] border-2 border-black">
         <div className="flex flex-col">
-            <div>{card.suit}</div>
-            <div>{card.rank}</div>
+            {/* <div>{card.suit}</div>
+            <div>{card.rank}</div> */}
+            <img
+              src={path_card}
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "contain",
+                display: "block",
+              }}
+            />
         </div>
     </div>
     )
--- src/client/src/components/qot/setup/SuitComponent.tsx
+++ src/client/src/components/qot/setup/SuitComponent.tsx
@@ -1,20 +1,25 @@
-import { Suit } from "games/qot/types"
-import { CardComponent } from "../general/CardComponent"
+import { Suit, Value } from "games/qot/types";
+import { CardComponent } from "../general/CardComponent";
 
 interface Props {
-    suit: Suit
+  suit: Suit;
 }
 
-export const SuitComponent = ({suit}: Props) => {
+export const SuitComponent = ({ suit }: Props) => {
+  /* select player's hand from redux */
+  // const hand = useAppSelector((state) => state.hand)
 
-    /* select player's hand from redux */
-    // const hand = useAppSelector((state) => state.hand)
-
-
-    return (
-        <div className="flex">
-            <div>{suit}</div>
-            <CardComponent card={{rank: 1, suit: suit}}/>
-        </div>
-    )
-}
+  return (
+    <div className="flex">
+      <div>{suit}</div>
+      {/* <CardComponent card={{ rank: 1, suit: suit }} /> */}
+      <div className="flex flex-wrap gap-2">
+        {Array.from({ length: 13 }, (_, i) => (
+          <div key={i} className="flex items-center gap-2">
+            <CardComponent card={{ rank: i + 1 as Value, suit: suit }} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
